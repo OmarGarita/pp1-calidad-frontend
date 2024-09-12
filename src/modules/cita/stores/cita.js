c0b4bb5afebf23b83f7f672f4352a7d36414b70f
FauricioTec <fagranados@estudiantec.cr>
2024-09-11 21:17:42 -0600

    Refactor appointment store and remove unused functions

--- src/modules/cita/stores/cita.js
+++ src/modules/cita/stores/cita.js
@@ -9,28 +9,9 @@ export const useAppointmentStore = defineStore("appointment", () => {
   const reports = ref([])
   const currentStudent = ref(null);
 
-  // Guardar la próxima cita en el estado
-  function saveNextAppointment(appointment) {
-    nextAppointment.value = appointment;
-  }
-  
   const setCurrentStudent = (student) => {
     currentStudent.value = student;
   };
-
-  // Obtener la próxima cita disponible
-  async function fetchNextAppointment(studentId, courseId, lastAppointmentId = null) {
-    const response = await axiosClient.get("/appointments/next", {
-      params: {
-        studentId,
-        courseId,
-        lastAppointmentId,
-      },
-    });
-    const appointment = response.data;
-    saveNextAppointment(appointment);
-    return appointment;
-  }
 
   async function getAppointmentReports(filtros) {
     const response = await axiosClient.get("/appointments/results", {
@@ -40,34 +21,12 @@ export const useAppointmentStore = defineStore("appointment", () => {
     return reports.value;
   }
 
-  // Aceptar una cita
-  async function acceptAppointment(appointmentId) {
-    const response = await axiosClient.patch(`/appointments/${appointmentId}/accept`);
-    const updatedAppointment = response.data;
-
-    const index = appointments.value.findIndex(
-      (appointment) => appointment.id === updatedAppointment.id
-    );
-    if (index !== -1) {
-      appointments.value[index] = updatedAppointment;
-    }
-
-    if (nextAppointment.value.id === updatedAppointment.id) {
-      saveNextAppointment(updatedAppointment);
-    }
-
-    return updatedAppointment;
-  }
+  
 
   return {
     appointments,
-    nextAppointment,
     currentStudent,
     setCurrentStudent,
-    saveNextAppointment,
     getAppointmentReports,
-    fetchNextAppointment,
-    acceptAppointment,
-
   };
 });
